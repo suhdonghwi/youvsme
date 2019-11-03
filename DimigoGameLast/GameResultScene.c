2d544b198b5ec9da26622bb2b8b6f08f960d2c92
suhdonghwi <hwidongsuh@gmail.com>
2019-11-03 16:01:44 +0900

    Add background image to game result scene

--- DimigoGameLast/GameResultScene.c
+++ DimigoGameLast/GameResultScene.c
@@ -2,12 +2,19 @@
 
 #include "CheeringPlayer.h"
 #include "GameScene.h"
+#include "SpriteResources.h"
+
+void on_render_game_result_scene(GameScene* scene, HDC main_dc) {
+  render_bitmap(cape_background_sprites[0], main_dc, (Pos){0, 0}, 1.0);
+}
 
 GameScene* create_game_result_scene(bool player1_won) {
   GameScene* scene = init_scene();
 
   GameObject* cheering_player = create_cheering_player(player1_won);
   insert_game_object(cheering_player, scene);
+
+  scene->on_render = on_render_game_result_scene;
 
   return scene;
 }
