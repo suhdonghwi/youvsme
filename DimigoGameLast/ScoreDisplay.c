e98457bc5ead2cda5da398f28b6844239d6390b7
suhdonghwi <hwidongsuh@gmail.com>
2019-11-09 14:04:49 +0900

    Embed DungGeunMo font

--- DimigoGameLast/ScoreDisplay.c
+++ DimigoGameLast/ScoreDisplay.c
@@ -7,11 +7,11 @@ extern int g_coco_score;
 extern int g_dingding_score;
 
 void on_render_score_display(GameObject* score_display, HDC main_dc) {
-  render_bitmap(score_board_sprites[0], main_dc, (Pos){280, 560}, 0.15);
+  // render_bitmap(score_board_sprites[0], main_dc, (Pos){280, 560}, 0.15);
 
   char score_str[100] = {'\0'};
-  sprintf(score_str, "%d           %d", g_coco_score, g_dingding_score);
-  TextOut(main_dc, 330, 630, score_str, strlen(score_str));
+  sprintf(score_str, "%d %d", g_coco_score, g_dingding_score);
+  TextOut(main_dc, 280, 560, score_str, strlen(score_str));
 }
 
 GameObject* create_score_display() {
--- DimigoGameLast/main.c
+++ DimigoGameLast/main.c
@@ -49,17 +49,18 @@ int main() {
 
   HBRUSH background_brush = GetStockObject(BLACK_BRUSH);
 
+  AddFontResource("DungGeunMo.ttf");
   HFONT score_font = CreateFont(50, 0, 0, 0, 0, 0, 0, 0, DEFAULT_CHARSET, 0, 0,
-                                0, VARIABLE_PITCH | FF_ROMAN,
-                                TEXT("DSEG7 Classic Mini-Bold Italic"));
+                                0, VARIABLE_PITCH | FF_ROMAN, TEXT("µÕ±Ù¸ð²Ã"));
   SelectObject(window_dc, score_font);
   SetBkMode(window_dc, TRANSPARENT);
   SetTextColor(window_dc, RGB(255, 0, 0));
 
-  GameScene* ready_scene = create_readystart_scene(
-      create_disk_game_scene(true, NULL), coco_turn_sprites, 3, (Pos){70, 100});
-  g_current_scene = create_game_help_scene(game_help_sprites[0], ready_scene);
-  // g_current_scene = create_disk_game_scene(true, NULL);
+  // GameScene* ready_scene = create_readystart_scene(
+  // create_disk_game_scene(true, NULL), coco_turn_sprites, 3, (Pos){70, 100});
+  // g_current_scene = create_game_help_scene(game_help_sprites[0],
+  // ready_scene);
+  g_current_scene = create_disk_game_scene(true, NULL);
   g_new_scene = NULL;
 
   while (1) {
