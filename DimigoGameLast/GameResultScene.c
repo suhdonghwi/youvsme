4f2b94590082562cbcaf6868c8e82ff765c1553b
suhdonghwi <hwidongsuh@gmail.com>
2019-11-14 22:26:37 +0900

    Add game result scene branching

--- DimigoGameLast/GameResultScene.c
+++ DimigoGameLast/GameResultScene.c
@@ -9,17 +9,26 @@
 extern int g_coco_score;
 extern int g_dingding_score;
 extern GameScene* g_new_scene;
+extern bool g_story_mode;
 
 void on_render_game_result_scene(GameScene* scene, HDC main_dc) {
   GameResultSceneData* data = scene->data;
 
   render_bitmap(background_sprites[1], main_dc, (Pos){0, 0}, 20.25);
 
-  char* press_enter = TEXT("[Enter]키를 눌러서 넘어가세요");
+  if (g_story_mode) {
+    char* press_enter = TEXT("[Enter]키를 눌러서 넘어가세요");
 
-  SelectObject(main_dc, data->font);
-  SetTextColor(main_dc, RGB(100, 100, 100));
-  TextOut(main_dc, 362, 632, press_enter, strlen(press_enter));
+    SelectObject(main_dc, data->font);
+    SetTextColor(main_dc, RGB(100, 100, 100));
+    TextOut(main_dc, 362, 632, press_enter, strlen(press_enter));
+  } else {
+    char* press_enter = TEXT("[Enter]키를 눌러서 메인 화면으로");
+
+    SelectObject(main_dc, data->font);
+    SetTextColor(main_dc, RGB(100, 100, 100));
+    TextOut(main_dc, 362, 552, press_enter, strlen(press_enter));
+  }
 
   if (is_pressed(VK_RETURN)) {
     g_new_scene = data->next_scene;
@@ -42,9 +51,11 @@ GameScene* create_game_result_scene(bool coco_won, GameScene* next_scene) {
   cheering_player->pos = (Pos){515, 320};
   insert_game_object(cheering_player, scene);
 
-  GameObject* score = create_score_display();
-  score->pos = (Pos){430, 500};
-  insert_game_object(score, scene);
+  if (g_story_mode) {
+    GameObject* score = create_score_display();
+    score->pos = (Pos){430, 500};
+    insert_game_object(score, scene);
+  }
 
   scene->on_render = on_render_game_result_scene;
   scene->on_first_render = on_first_render_game_result_scene;
